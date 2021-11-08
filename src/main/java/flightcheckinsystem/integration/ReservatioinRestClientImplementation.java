57e2ca9ba162154c1b2a4922d869e4167271bcea
Faheem Shah <shahfaheem880@gmail.com>
2021-11-08 18:49:23 +0500

    Make functions for finding and update Rerservation Request In ReservationRestClientImplementation Class

---
+++ src/main/java/flightcheckinsystem/integration/ReservatioinRestClientImplementation.java
@@ -0,0 +1,20 @@
+package flightcheckinsystem.integration;
+
+import flightcheckinsystem.integration.dto.Reservation;
+import flightcheckinsystem.integration.dto.ReservationUpdateRequest;
+
+public class ReservatioinRestClientImplementation implements ReservationRestClient {
+
+	@Override
+	public Reservation findReservation(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation updateResrvation(ReservationUpdateRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
