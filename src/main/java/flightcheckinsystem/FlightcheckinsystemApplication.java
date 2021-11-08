ea56578406480232e7066a133872ae285618ff0e
Faheem Shah <shahfaheem880@gmail.com>
2021-11-08 17:19:41 +0500

    MAke new Project for Flight CheckIN System

---
+++ src/main/java/flightcheckinsystem/FlightcheckinsystemApplication.java
@@ -0,0 +1,13 @@
+package flightcheckinsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightcheckinsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightcheckinsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flightcheckinsystem/FlightcheckinsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package flightcheckinsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightcheckinsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
