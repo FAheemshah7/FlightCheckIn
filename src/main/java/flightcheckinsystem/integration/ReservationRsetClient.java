0fe1f97069d89bb0bf03221bd5bbfc1978901f72
Faheem Shah <shahfaheem880@gmail.com>
2021-11-08 17:43:08 +0500

    Copied Flight,PAssenger,Reservation and ReservationUpdateResquest Class From FlightReservation Project

--- src/main/java/flightcheckinsystem/integration/ReservationRsetClient.java
+++ src/main/java/flightcheckinsystem/integration/ReservationRsetClient.java
@@ -1,5 +1,10 @@
-package flightcheckinsystem.integration;
+ package flightcheckinsystem.integration;
 
 public interface ReservationRsetClient {
+	
+	public Reservation findReservation(int id );
+	public Reservation updateResrvation(int id );
+	
+	
 
 }
---
+++ src/main/java/flightcheckinsystem/integration/dto/Reservation.java
@@ -0,0 +1,46 @@
+package flightcheckinsystem.integration.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="tReservation")
+public class Reservation extends AbstractEntity {
+	
+	private Boolean CHECKED_IN;
+	private int NUMBER_OF_BAGS;
+	@OneToOne
+	private Passenger passenger;
+	@OneToOne
+	private Flight flight;
+	
+	
+
+	public Boolean getCHECKED_IN() {
+		return CHECKED_IN;
+	}
+	public void setCHECKED_IN(Boolean cHECKED_IN) {
+		CHECKED_IN = cHECKED_IN;
+	}
+	public int getNUMBER_OF_BAGS() {
+		return NUMBER_OF_BAGS;
+	}
+	public void setNUMBER_OF_BAGS(int nUMBER_OF_BAGS) {
+		NUMBER_OF_BAGS = nUMBER_OF_BAGS;
+	}
+	public Passenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}	
+
+}
